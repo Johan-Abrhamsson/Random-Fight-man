b37e83406a100d66005c0d783cf9bcddfbc73316
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2020-10-15 10:43:39 +0200

    First Commit

---
+++ Fight MAN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fight_MAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
